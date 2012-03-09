cda1044c9d3f1e34b116c0b0aed53af5a9e4b8a4
Jonathan Harker <jesusaur@cat.pdx.edu>
2012-03-09 15:51:53 -0800

    Add an apache reverse proxy pointing at rstudio.

--- manifests/apacheconfig.pp
+++ manifests/apacheconfig.pp
@@ -1,4 +1,9 @@
 class rstudio::apacheconfig {
     include apache
 
+    file {
+        "/etc/apache2/httpd.conf":
+            ensure  => present,
+            content => template("rstudio/httpd.erb"),
+    }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,13 @@
 class rstudio::params {
 
     # The port the web server will listen on,
-    # the default is 80
-    $port = 80    
+    # the default is 8787
+    $port = 8787    
     
     # The maximum file size to upload (in MB)
     $size = 100
+
+    # The admin email for the apache reverse proxy
+    $email = root@cat.pdx.edu
 
 }
