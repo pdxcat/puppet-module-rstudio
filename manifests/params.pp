084a5afd0279420917f2fa751b2f1120ff5ee31d
Jonathan Harker <jesusaur@cat.pdx.edu>
2012-03-09 19:39:28 -0800

    Fiddle fiddle

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,12 +2,12 @@ class rstudio::params {
 
     # The port the web server will listen on,
     # the default is 8787
-    $port = 8787    
+    $::rstudio::port = 8787    
     
     # The maximum file size to upload (in MB)
-    $size = 100
+    $::rstudio::size = 100
 
     # The admin email for the apache reverse proxy
-    $email = 'root@cat.pdx.edu'
+    $::rstudio::email = 'root@cat.pdx.edu'
 
 }
