f841e18f9301f7630ca550a56d1e965cdd37321e
Jonathan Harker <jesusaur@cat.pdx.edu>
2012-03-30 17:25:33 -0700

    Revert "Lets try hard coded strings"
    This reverts commit f6675575e1fa6dfb25fd49080c4a8b1295565d2c.
    
    Conflicts:
    
    	manifests/proxy.pp

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -1,17 +1,18 @@
 class rstudio::proxy {
 
+    include rstudio::params
     include nginx
 
     nginx::site {
       "rstudio-proxy":
-        domain              => 'barbados.cat.pdx.edu',
+        domain              => $::fqdn,
         ssl                 => true,
-        ssl_certificate     => "/var/lib/ssl/barbados.cat.pdx.edu.crt",
-        ssl_certificate_key => "/var/lib/ssl/barbados.cat.pdx.edu.key",
+        ssl_certificate     => inline_template("/var/lib/ssl/<%= @fqdn %>.crt"),
+        ssl_certificate_key => inline_template("/var/lib/ssl/<%= @fqdn %>.key"),
         keepalive           => 120,
         proxy               => true,
         proxy_domain        => '127.0.0.1',
-        proxy_port          => '8787',
+        proxy_port          => $::rstudio::params::port,
     }
 
 }
