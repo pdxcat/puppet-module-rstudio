c63492033b419012cd943032cc7792edee121f61
Reid Vandewiele <marut@cat.pdx.edu>
2012-04-05 11:35:24 -0700

    Convert to singleton_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,39 @@
 class rstudio {
-    include package::virtual
 
-    # Set up two proxies:
-    #   The first forwards port 80 to 443
-    #   The second forwards port 443 to rstudio
-    include rstudio::proxy
+  # Set up two proxies:
+  #   The first forwards port 80 to 443
+  #   The second forwards port 443 to rstudio
+  include rstudio::proxy
 
+  singleton_resources(
+    Package['r-base'],
+    Package['rstudio-server'],
+  )
 
-    realize(
-        Package['r-base'],
-        Package['rstudio-server'],
-    )
+  file {
+    "/etc/rstudio/rserver.conf":
+      ensure  => file,
+      content => template("rstudio/rserver.erb"),
+  }
 
-    file {
-        "/etc/rstudio/rserver.conf":
-            ensure  => file,
-            content => template("rstudio/rserver.erb"),
-    }
+  file {
+    "/etc/rstudio/rsession.conf":
+      ensure  => file,
+      content => template("rstudio/rsession.erb"),
+  }
 
-    file {
-        "/etc/rstudio/rsession.conf":
-            ensure  => file,
-            content => template("rstudio/rsession.erb"),
-    }
-
-    service {
-        "rstudio-server":
-            provider     => $operatingsystem ? {
-                'Ubuntu' => 'upstart',
-                default  => undef,
-            },
-            ensure      => 'running',
-            require     => Package['rstudio-server'],
-            subscribe   => [
-                File['/etc/rstudio/rserver.conf'],
-                File['/etc/rstudio/rsession.conf'],
-            ],
-    }
+  service {
+    "rstudio-server":
+      provider   => $operatingsystem ? {
+        'Ubuntu' => 'upstart',
+        default  => undef,
+      },
+      ensure     => 'running',
+      require    => Package['rstudio-server'],
+      subscribe  => [
+        File['/etc/rstudio/rserver.conf'],
+        File['/etc/rstudio/rsession.conf'],
+      ],
+  }
 
 }
